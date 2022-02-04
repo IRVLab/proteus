01f197a6dbf69fa0f05deb3ee0afff73939df7f0
Michael Fulton <fulto081@umn.edu>
2022-02-04 00:18:08 -0600

    Added different types of KNodes to allow for directional kinemes

--- src/proteus/kineme.py
+++ src/proteus/kineme.py
@@ -5,15 +5,31 @@ from proteus.velocity import Velocity
 from proteus.duration import Duration
 
 class KNode(Node):
-    def __init__(self, id, desc, pos, orr, velo, dur):
+    def __init__(self, id, desc, dur):
         super().__init__(id, desc)
-        self.position = pos
-        self.orientation = orr
-        self.velocity = velo
         self.duration = dur
 
     def __str__(self):
-        return "{0} POS({1.position}) ORR({1.orientation}) VEL({1.velocity}) DUR({1.duration})".format(super().__str__(), self)
+        return "{0} DUR({1.duration})".format(super().__str__(), self)
+
+class KNodeAbsolute(KNode):
+    def __init__(self, id, desc, pos, orr, velo, dur):
+        super().__init__(id, desc, dur)
+        self.position = pos
+        self.orientation = orr
+        self.velocity = velo
+
+    def __str__(self):
+        return "{0} POS({1.position}) ORR({1.orientation}) VEL({1.velocity}) ".format(super().__str__(), self)
+
+class KNodeDirectional(KNode):
+    def __init__(self, id, desc, mode, velo, dur):
+        super().__init__(id, desc, dur)
+        self.mode = mode
+        self.velocity = velo
+
+    def __str__(self):
+        return "{0} MODE ({1.mode}) VEL({1.velocity}) ".format(super().__str__(), self)
             
 class Kineme(object):
     def __init__(self):
@@ -38,12 +54,31 @@ class Kineme(object):
 
         # Now we have to parse the KNodes.
         for n in xml_object:
-            step = int(n.get('step'))
-            description = n.get('description')
+            type = n.tag
+            if type == 'knode-abs':
+                step = int(n.get('step'))
+                description = n.get('description')
 
-            pos = Position(n[0].get('x'), n[0].get('y'), n[0].get('z'))
-            orr = Orientation(n[1].get('roll'), n[1].get('pitch'), n[1].get('yaw'))
-            vel = Velocity(n[2].get('linear'),n[2].get('angular'))
-            dur = Duration(n[3].get('seconds'))
+                pos = Position(n[0].get('x'), n[0].get('y'), n[0].get('z'))
+                orr = Orientation(n[1].get('roll'), n[1].get('pitch'), n[1].get('yaw'))
+                vel = Velocity(n[2].get('linear'),n[2].get('angular'))
+                dur = Duration(n[3].get('seconds'))
 
-            self.knodes.append(KNode(step, description, pos, orr, vel, dur))
+                self.knodes.append(KNodeAbsolute(step, description, pos, orr, vel, dur))
+
+            elif type == 'knode-dir': 
+                step = int(n.get('step'))
+                description = n.get('description')
+
+                mode = n[0].get('type')
+                vel = Velocity(n[1].get('linear'),n[1].get('angular'))
+                dur = Duration(n[2].get('seconds'))
+
+                self.knodes.append(KNodeDirectional(step, description, mode, vel, dur))
+
+            elif type == 'knode-tar': 
+                pass
+            else:
+                print("NO KNODE TYPE RECOGNIZED.")
+            
+
