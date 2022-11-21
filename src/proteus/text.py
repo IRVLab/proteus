aad9ffa2771438df2e3deb8b77be4f3a73beed08
Michael Fulton <fulto081@umn.edu>
2022-11-21 17:26:46 -0600

    loco deploy updates

--- src/proteus/text.py
+++ src/proteus/text.py
@@ -1,6 +1,6 @@
 class Text(object):
-    def __init__(self, data):
-        self.data = str(data)
+    def __init__(self):
+        self.data = ""
 
     def __str__(self):
         return "TEXT {}".format(self.data)
