aa199ad8eb6247d8d6ffd4c945de8476f7fd3e9f
Michael Fulton <fulto081@umn.edu>
2022-12-07 18:36:22 -0600

    smol change to diver class

--- src/proteus/environment/agent/diver.py
+++ src/proteus/environment/agent/diver.py
@@ -1,7 +1,9 @@
 from proteus.environment.agent import Agent
 
 class Diver(Agent):
-    def __init__(self):
+    def __init__(self, id):
         super().__init__(self)
+        self.readable_id = id
         self.mobile = True
         self.intelligent = True
+        
